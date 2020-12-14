c72b19c355379dab551deb07602fbc91fe941e87
tatsuya-ss <72326299+tatsuya-ss@users.noreply.github.com>
2020-12-14 23:25:36 +0900

    tabBarを使ったレイアウトの作成。

---
+++ kadai7/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  kadai7
+//
+//  Created by 坂本龍哉 on 2020/12/14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet var textField1: UITextField!
+    @IBOutlet var textField2: UITextField!
+    @IBOutlet var answerLabel: UILabel!
+    @IBOutlet var tabBar: UITabBar!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    
+
+}
+
